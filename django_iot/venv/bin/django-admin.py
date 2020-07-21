264e85b4db5fa35bb70b91fee2001b4d4ed875b9
Jongyeon Yoon <0417yjy@naver.com>
2020-07-21 13:17:33 +0900

    Reinstall venv and django

--- django_iot/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/jongyeon/2020-1-CECD3-Generator-8-master/django_iot/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
