2674545ba2826e7f4e612466eac1aa943c991d4a
Woojoo1602 <woojoo1602@gmail.com>
2020-07-30 20:43:18 +0900

    Changing Model.py

--- django_iot/logtable/models.py
+++ django_iot/logtable/models.py
@@ -1,8 +1,34 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+import uuid
+
 
 # Create your models here.
+
+class Building(models.Model): # Model for Building list displayed in main page.
+    building_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable = False)
+    building_name = models.CharField(max_length=15)
+    levels = models.IntegerField()
+
+class Level(models.Model):
+    level_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable = False)
+    level_num = models.IntegerField()
+    img_file_path = models.CharField(max_length=200)
+    building_id = models.ForeignKey('Building', on_delete=models.CASCADE)
+
+class Sensor(models.Model):
+    TYPE_CHOICES = (
+        ('IG','Intergration'),
+        ('RD','Radar'),
+        ('RF','RF')
+    )
+    sensor_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable = False)
+    sensor_type = models.CharField(max_length=2, choices = TYPE_CHOICES)
+    sensor_status = models.ForeignKey('Log',on_delete=models.CASCADE)
+    level_id = models.ForeignKey('Level', on_delete=models.CASCADE)
+    #position = models.CommaSeparatedIntegerField(max_length=10)
+
 class Log(models.Model): # Model for Logs displayed in main page.
     STATUS_CHOICES = (
         ('OP', 'Operational'),
@@ -10,30 +36,11 @@ class Log(models.Model): # Model for Logs displayed in main page.
         ('BR', 'Broken'),
         ('ND', 'Not Defined')
     ) # Has 4 sensor status choices
-
     sensor_name = models.CharField(max_length=10, db_index=True, primary_key=True)
     updated_time = models.DateTimeField(default=timezone.now)
+    sensor_id = models.ForeignKey('Sensor',on_delete=models.CASCADE)
     status=models.CharField(
         max_length=2,
         choices=STATUS_CHOICES,
         default='ND'
     )
-
-class Building(models.Model): # Model for Building list displayed in main page.
-    building_name = models.CharField(max_length=15, primary_key=True)
-    levels = models.IntegerField()
-"""
-class Level(models.Model):
-    level_id = models.CharField(max_length=10, db_index=True, primary_key=True)
-    level_num = models.IntegerField()
-    img_file_path = models.CharField(max_length=200)
-
-class Sensor(models.Model):
-    sensor_id = models.CharField(max_length=10, db_index=True, primary_key=True)
-    sensor_type = models.CharField(max_length=2, choice = TYPE_CHOICES)
-    TYPE_CHOICES = (
-        ('IG','Intergration'),
-        ('RD','Radar'),
-        ('RF','RF')
-    )
-"""
