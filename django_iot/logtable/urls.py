93df12872b3f6361c755b73d86274dd20855d531
Jongyeon Yoon <0417yjy@naver.com>
2020-07-31 16:17:04 +0900

    Delete unused functions

--- django_iot/logtable/urls.py
+++ django_iot/logtable/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('', views.log_list, name='log_list'),
     path('', views.dashboard, name='dashboard'),
 ]
--- django_iot/logtable/views.py
+++ django_iot/logtable/views.py
@@ -2,12 +2,6 @@ from django.shortcuts import render
 from .models import Log
 from .tables import LogTable
 
-# Create your views here.
-def log_list(request):
-    logs = Log.objects.all() # get all logs
-    table = LogTable(logs) # make a table by logs
-    return render(request, 'logtable/log_list.html', {'table':table}) # render table
-
 def dashboard(request):
     logs = Log.objects.all() # get all logs
     table = LogTable(logs) # make a table by logs
