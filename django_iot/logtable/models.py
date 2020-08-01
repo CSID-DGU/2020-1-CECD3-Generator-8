51b4543adfc8f097a1003eebc19c00c1ae5c4e03
Woojoo1602 <woojoo1602@gmail.com>
2020-08-01 22:20:09 +0900

    Eureka!
    Chanege models.py and complete migration

--- django_iot/logtable/models.py
+++ django_iot/logtable/models.py
@@ -15,7 +15,7 @@ class Level(models.Model):
     level_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable = False)
     level_num = models.IntegerField()
     img_file_path = models.CharField(max_length=200)
-    building_id = models.ForeignKey('Building', on_delete=models.CASCADE)
+   # building_id = models.ForeignKey('Building', on_delete=models.CASCADE,default = uuid.uuid4)
 
 class Sensor(models.Model):
     TYPE_CHOICES = (
@@ -31,16 +31,16 @@ class Sensor(models.Model):
     ) # Has 4 sensor status choices
     sensor_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable = False)
     sensor_type = models.CharField(max_length=2, choices = TYPE_CHOICES)
-    sensor_status=models.CharField(max_length=2, choices=STATUS_CHOICES, default='ND')
+    sensor_status=models.CharField(max_length=2, choices=STATUS_CHOICES, default='ND', unique = True)
     level_id = models.ForeignKey('Level', on_delete=models.CASCADE)
     #position = models.CommaSeparatedIntegerField(max_length=10)
 
-# class recent_Log(Log):
-#     pass
 
 class Log(models.Model): # Model for Logs displayed in main page.
     log_id = models.AutoField(primary_key=True)
     sensor_name = models.CharField(max_length=10, db_index=True )
     updated_time = models.DateTimeField(default=timezone.now)
-    sensor_id = models.ForeignKey('Sensor',on_delete=models.CASCADE)
+    sensor_id = models.ForeignKey('Sensor',on_delete=models.CASCADE, to_field='sensor_id', default= uuid.uuid4 )
+    status = models.ForeignKey('Sensor', on_delete=models.CASCADE, to_field='sensor_status', related_name='status', default='ND')
+    
     
