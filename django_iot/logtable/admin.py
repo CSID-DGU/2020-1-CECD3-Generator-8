ae92bf5f4b9c3f10ceba2612cfe09a0a597255f0
Jongyeon Yoon <0417yjy@naver.com>
2020-08-03 16:29:32 +0900

    Add import/export function to 'Log' in admin site

--- django_iot/logtable/admin.py
+++ django_iot/logtable/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 from .models import Building, Level, Log, Sensor
 
 # Register your models here.
-admin.site.register(Log)
+@admin.register(Log)
+class LogAdmin(ImportExportModelAdmin):
+    pass
 admin.site.register(Building)
 admin.site.register(Level)
 admin.site.register(Sensor)
