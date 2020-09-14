e97614c7b5f2f9a1c5ee4c0e80f56d683e9377fd
Jongyeon Yoon <0417yjy@naver.com>
2020-09-15 02:02:43 +0900

    Make extending structure in html templates

--- django_iot/iotsite/settings.py
+++ django_iot/iotsite/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'iotsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'iotsite/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
