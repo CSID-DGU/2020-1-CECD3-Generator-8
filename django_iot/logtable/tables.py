0298e1fdf3d3af0debf4bc766eb24e02dd43523d
Jongyeon Yoon <0417yjy@naver.com>
2020-09-15 17:54:20 +0900

    Add monitoring template (checkbox function not implemented)

--- django_iot/logtable/tables.py
+++ django_iot/logtable/tables.py
@@ -1,14 +1,30 @@
 import django_tables2 as tables
-from .models import Log
+from .models import Log, Sensor
+
 
 class LogTableQuerySet(tables.Table):
-    sensor_code_column = tables.Column(accessor='get_sensor_code', verbose_name='Sensor Code')
-    sensor_name_column = tables.Column(accessor='get_sensor_name', verbose_name='Sensor Name')
-    sensor_type_column = tables.Column(accessor='get_sensor_type', verbose_name='Sensor Type')
-    sensor_status_column = tables.Column(accessor='get_sensor_status', verbose_name='Status')
+    sensor_code_column = tables.Column(
+        accessor='get_sensor_code', verbose_name='Sensor Code')
+    sensor_name_column = tables.Column(
+        accessor='get_sensor_name', verbose_name='Sensor Name')
+    sensor_type_column = tables.Column(
+        accessor='get_sensor_type', verbose_name='Sensor Type')
+    sensor_status_column = tables.Column(
+        accessor='get_sensor_status', verbose_name='Status')
 
     class Meta:
-        fields = ['id', 'sensor_code_column', 'sensor_name_column', 'sensor_type_column','updated_time', 'sensor_status_column']
+        fields = ['id', 'sensor_code_column', 'sensor_name_column',
+                  'sensor_type_column', 'updated_time', 'sensor_status_column']
         model = Log
         order_by = 'sensor_code_column'
+        template_name = "django_tables2/bootstrap.html"
+
+
+class MonitoringTableQuerySet(tables.Table):
+    checkbox_column = tables.columns.CheckBoxColumn(accessor='pk')
+
+    class Meta:
+        fields = ['checkbox_column', 'id', 'sensor_code', 'sensor_name',
+                  'sensor_type', 'updated_time', 'sensor_status']
+        model = Sensor
         template_name = "django_tables2/bootstrap.html"
--- django_iot/logtable/views.py
+++ django_iot/logtable/views.py
@@ -2,15 +2,27 @@ from django.shortcuts import render
 from django.core import serializers
 from django.http import HttpResponse
 from django.db.models import Max
-from .models import Log
-from .tables import LogTableQuerySet
+from .models import Log, Sensor
+from .tables import LogTableQuerySet, MonitoringTableQuerySet
+
 
 def dashboard(request):
-    logs = Log.objects.raw('SELECT "logtable_log"."id", "logtable_log"."sensor_id", "logtable_log"."updated_time" FROM "logtable_log" GROUP BY "sensor_id"')
-    table = LogTableQuerySet(logs) # make a table by logs
-    return render(request, 'logtable/dashboard.html', {'table':table}) # render table
+    logs = Log.objects.raw(
+        'SELECT "logtable_log"."id", "logtable_log"."sensor_id", "logtable_log"."updated_time" FROM "logtable_log" GROUP BY "sensor_id"')
+    table = LogTableQuerySet(logs)  # make a table by logs
+    # render table
+    return render(request, 'logtable/dashboard.html', {'table': table})
+
 
 def json(request):
-    logs = Log.objects.all() # get all logs
+    logs = Log.objects.all()  # get all logs
     log_list = serializers.serialize('json', logs)
     return HttpResponse(log_list, content_type="text/json-comment-filtered")
+
+
+def monitoring(request):
+    sensors_with_problems = Sensor.objects.filter(is_handled='False')
+    table = MonitoringTableQuerySet(
+        sensors_with_problems)  # make a table by sensor queryset
+    # render table
+    return render(request, 'logtable/monitoring.html', {'table': table})
