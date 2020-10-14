43e019a588612bd41ba16a4de66675182d5750a3
Jongyeon Yoon <0417yjy@naver.com>
2020-10-14 11:59:24 +0000

    Commit changes in pythonanywhere server

--- django_iot/iotsite/settings.py
+++ django_iot/iotsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@6*t$$*sl2+r%dgu4b=7th17d%e3&rqdaxws%sja1hhg+or1(7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1','[::1]','.pythonanywhere.com']
 
 
 # Application definition
