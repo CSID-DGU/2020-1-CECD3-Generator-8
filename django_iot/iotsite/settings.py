1fc43449ded1b0a312039eb239e5a6e6a8d1ee85
WooJoo <woojoo1602@gmail.com>
2020-10-31 12:07:41 +0900

    Delete pythonanywhere url

--- django_iot/iotsite/settings.py
+++ django_iot/iotsite/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','[::1]','.pythonanywhere.com', '.amazonaws.com']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','[::1]','.amazonaws.com']
 
 
 # Application definition
