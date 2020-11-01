8e8239a1f579804d6bdcdce2693068fc0636981d
Jongyeon Yoon <0417yjy@naver.com>
2020-11-01 15:52:01 +0900

    Update GET request url

--- django_iot/logtable/urls.py
+++ django_iot/logtable/urls.py
@@ -7,10 +7,11 @@ from . import views
 urlpatterns = [
     path('', lambda request: redirect('dashboard', permanent=True)),
     path('dashboard', views.dashboard, name='dashboard'),
-    path('dashboard/sme20u/<slug:sensor_code>', views.get_sme20u_data_in_json, name='json'), # http response url (센서 데이터 제공)
     path('dashboard/export', views.dashboard_export, name='export_file_dashboard'),
     path('monitoring', views.monitoring, name='monitoring'),
-    path('monitoring/export', views.monitoring_export,name='export_file_monitoring'),
+    path('monitoring/export', views.monitoring_export, name='export_file_monitoring'),
     path('<int:b_id>/<slug:l_num>/', views.floor, name='floor'),
+    path('sme20u/<slug:sensor_code>/json', views.get_sme20u_data_in_json, name='json'), # http response url (센서 데이터 제공)
+    #path('sme20u/<slug:sensor_code>/delete', views_), # for delete monitoring row
 ]
 static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
